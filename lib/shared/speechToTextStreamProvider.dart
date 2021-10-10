2abd6a95dca0f955eed407983858157bbfdc5acf
DudeBro249 <appdevdeploy@gmail.com>
2021-10-10 13:15:37 +0530

    :sparkles: feat: speech to text

--- lib/shared/speechToTextStreamProvider.dart
+++ lib/shared/speechToTextStreamProvider.dart
@@ -6,23 +6,39 @@ import 'package:speech_to_text/speech_recognition_result.dart';
 
 final speechToTextStreamProvider =
     StreamProvider.autoDispose<SpeechRecognitionResult?>((ref) async* {
-  final speechToTextService =
-      await ref.watch(speechToTextServiceProvider.future);
+  final speechToTextService = ref.watch(speechToTextServiceProvider);
 
   final streamController = StreamController<SpeechRecognitionResult?>();
   streamController.add(null);
 
-  if (!speechToTextService.isListening) {
-    speechToTextService.listen(
-      onResult: (result) {
-        print('log: got speech result');
-        streamController.add(result);
+  if (!speechToTextService.isAvailable) {
+    print('log: initializing speech to text service');
+    await speechToTextService.initialize(
+      onStatus: (status) {
+        print('log: listening status is: $status');
       },
+      onError: (val) => print('onError: $val'),
     );
   }
 
-  ref.onDispose(() {
-    speechToTextService.stop();
+  if (!speechToTextService.isListening) {
+    await speechToTextService.listen(
+      onDevice: true,
+      onResult: (result) {
+        print('log: got speech result');
+        print('the result is: ${result.recognizedWords}');
+        streamController.add(result);
+      },
+    );
+  } else {
+    print('log: stopping listening...');
+    await speechToTextService.stop();
+  }
+
+  ref.onDispose(() async {
+    if (speechToTextService.isListening) {
+      await speechToTextService.stop();
+    }
     streamController.close();
   });
 
